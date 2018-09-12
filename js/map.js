69f30bb8d353e9fa8ad54af41655c5d351707ca0
Andrey Bulaev <bulayka89@gmail.com>
2018-09-12 19:03:02 +0300

    Travis

--- js/map.js
+++ js/map.js
@@ -96,15 +96,15 @@ var getRandomArrayList = function (array, length) {
 };
 
 function shuffleArray(array) {
-    for (var i = array.length - 1; i > 0; i--) {
-        var j = Math.floor(Math.random() * (i + 1));
-        var temp = array[i];
-        array[i] = array[j];
-        array[j] = temp;
-    }
+  for (var i = array.length - 1; i > 0; i--) {
+    var j = Math.floor(Math.random() * (i + 1));
+    var temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+  }
 
-    return array;
-};
+  return array;
+}
 
 var getAvatarUrl = function (index) {
   return 'img/avatars/user0' + index + '.png';
